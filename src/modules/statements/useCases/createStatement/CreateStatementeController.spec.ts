2bff530fc3493f4c38391ed986f161e2e9e0be7b
HaisonOhara <haison.ohara@outlook.com>
2022-03-22 22:19:40 -0300

    initialize test structure

---
+++ src/modules/statements/useCases/createStatement/CreateStatementeController.spec.ts
@@ -0,0 +1,9 @@
+describe("Create Statement Use Case Test", () => {
+  beforeEach(() => {
+    // carsRepositoryInMemory = new CarsRepositoryInMemory();
+    // createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+  it("should be able return something", async () => {
+    expect(true).toBe(true);
+  });
+});
